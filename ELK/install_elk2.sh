ef58fc8df823480880f47de42f3964e21ca15001
Vitalii <vitalii.stepchuk@sidmar.ch>
2024-06-23 11:36:51 +0300

    Update install_elk2.sh

--- ELK/install_elk2.sh
+++ ELK/install_elk2.sh
@@ -3,4 +3,5 @@ apt-get install apt-transport-https
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
 apt-get update && apt-get install elasticsearch kibana openjdk-8-jre logstash
 
+ss -tunlp | grep 9200
 curl http://localhost:9200
