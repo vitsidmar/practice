543573d4fddc676156815281fa989b193df3af76
Vitalii <vitalii.stepchuk@sidmar.ch>
2024-06-23 11:34:58 +0300

    Create install_elk2.sh

---
+++ ELK/install_elk2.sh
@@ -0,0 +1,5 @@
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+apt-get install apt-transport-https
+echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
+apt-get update && apt-get install elasticsearch kibana openjdk-8-jre logstash
+
