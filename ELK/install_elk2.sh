96a8696f989c1d9ba60683c59ed200d99f646a6e
Vitalii <vitalii.stepchuk@sidmar.ch>
2024-06-23 11:41:18 +0300

    Update install_elk2.sh

--- ELK/install_elk2.sh
+++ ELK/install_elk2.sh
@@ -2,7 +2,7 @@ wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
 apt-get update && apt-get install apt-transport-https elasticsearch kibana openjdk-8-jre logstash -y
 elastic_conf="/etc/elasticsearch/elasticsearch.yml"
-sudo sed -i "s|#node.name: node-1|node.name: elk-node|" $elastic_conf
-sudo sed -i "s|#network.host: 192.168.0.1|network.host: 127.0.0.1|" $elastic_conf
+sed -i "s|#node.name: node-1|node.name: elk-node|" $elastic_conf
+sed -i "s|#network.host: 192.168.0.1|network.host: 127.0.0.1|" $elastic_conf
 ss -tunlp | grep 9200
 curl http://localhost:9200
