5ca2d4447ef80a4e61187ab511b5a346932b2635
Vitalii <vitalii.stepchuk@sidmar.ch>
2024-06-24 16:34:10 +0300

    Update install_elk.sh

--- ELK/install_elk.sh
+++ ELK/install_elk.sh
@@ -74,7 +74,7 @@ cat >$logstash_dir/output.conf <<EOF
 output {
         elasticsearch {
             hosts => "https://localhost:9200"
-            index => "websrv-%{+YYYY.MM}"
+            index => "winlogbeat-%{+YYYY.MM}"
 	    user => "elastic"
 	    password => "$elastic_password"
 	    cacert => "/etc/logstash/certs/http_ca.crt"
