3eff165eaf9104ee022f6f4ba7ca4d38f233fbb8
Vitalii <vitalii.stepchuk@sidmar.ch>
2024-06-26 13:55:31 +0300

    Update add_user_AD.ps1

--- AD/add_user_AD.ps1
+++ AD/add_user_AD.ps1
@@ -1,5 +1,5 @@
 # CSV: Login; Password; LastName; FirstName; MiddleName; OU; JobTitle
-# powershell -ExecutionPolicy Bypass -File C:\Path\To\add_user_AD.ps1
+# powershell -ExecutionPolicy Bypass -File C:\User\vital\Downloads\add_user_AD.ps1
 Import-Module ActiveDirectory
 
 # Funktion für die Auswahl einer Datei über ein Dialogfenster
