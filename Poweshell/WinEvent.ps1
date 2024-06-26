7f9363133cba072944b53fa1804bafba95fe971d
Vitalii <vitalii.stepchuk@sidmar.ch>
2024-06-26 15:41:00 +0300

    Update WinEvent.ps1

--- Poweshell/WinEvent.ps1
+++ Poweshell/WinEvent.ps1
@@ -2,29 +2,28 @@
 $startDate = (Get-Date).AddDays(-7)  # Letzten 7 Tage
 $endDate = Get-Date
 
-try {
-    # Abfrage des Ereignisprotokolls zur Ermittlung fehlgeschlagener Anmeldeversuche (Ereignis-ID 4625)
-    $events = Get-WinEvent -FilterHashtable @{
-        LogName = 'Security'
-        Id = 4625
-        StartTime = $startDate
-        EndTime = $endDate
-    } -ErrorAction SilentlyContinue | Select-Object TimeCreated, Id, Message
+# Abfrage des Ereignisprotokolls zur Ermittlung fehlgeschlagener Anmeldeversuche (Ereignis-ID 4625)
+$events = Get-WinEvent -FilterHashtable @{
+    LogName = 'Security'
+    Id = 4625
+    StartTime = $startDate
+    EndTime = $endDate
+} -ErrorAction SilentlyContinue
 
-    # Überprüfen der Anzahl der gefundenen Ereignisse
-    if ($events -eq $null -or $events.Count -eq 0) {
-        Write-Host "Keine fehlgeschlagenen Anmeldeversuche in den letzten 7 Tagen."
-    } else {
-        # Ausgabe der Anzahl fehlgeschlagener Anmeldeversuche
-        $totalFailedAttempts = $events.Count
-        Write-Host "Anzahl fehlgeschlagener Anmeldeversuche in den letzten 7 Tagen: $totalFailedAttempts"
-
-        # Erstellen des Berichts als CSV-Datei
-        $reportPath = "C:\Temp\FailedLogonAttemptsReport.csv"
-        $events | Export-Csv -Path $reportPath -NoTypeInformation
-
-        Write-Host "Bericht erstellt unter: $reportPath"
-    }
-} catch {
+# Überprüfen der Anzahl der gefundenen Ereignisse
+if ($events -eq $null -or $events.Count -eq 0) {
     Write-Host "Keine fehlgeschlagenen Anmeldeversuche in den letzten 7 Tagen."
+} else {
+    # Auswahl der relevanten Eigenschaften
+    $events = $events | Select-Object TimeCreated, Id, Message
+    
+    # Ausgabe der Anzahl fehlgeschlagener Anmeldeversuche
+    $totalFailedAttempts = $events.Count
+    Write-Host "Anzahl fehlgeschlagener Anmeldeversuche in den letzten 7 Tagen: $totalFailedAttempts"
+
+    # Erstellen des Berichts als CSV-Datei
+    $reportPath = "C:\Temp\FailedLogonAttemptsReport.csv"
+    $events | Export-Csv -Path $reportPath -NoTypeInformation
+
+    Write-Host "Bericht erstellt unter: $reportPath"
 }
