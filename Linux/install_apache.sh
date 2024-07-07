242a88d45dab6436f4115a4c2e5f631bd84dca78
Vitalii <vitalii.stepchuk@sidmar.ch>
2024-07-07 10:33:26 +0300

    Update install_apache.sh

--- Linux/install_apache.sh
+++ Linux/install_apache.sh
@@ -1,33 +1,38 @@
 apt update -y && apt upgrade -y
 apt install apache2 openssl
-sudo mkdir /etc/apache2/ssl
-sudo openssl genpkey -algorithm RSA -out /etc/apache2/ssl/server.key -pkeyopt rsa_keygen_bits:2048
-sudo openssl req -new -key /etc/apache2/ssl/server.key -out /etc/apache2/ssl/server.csr
-sudo openssl x509 -req -days 365 -in /etc/apache2/ssl/server.csr -signkey /etc/apache2/ssl/server.key -out /etc/apache2/ssl/server.crt
-
+domain="lamp.migrate.local"
+ssldir="/etc/apache2/ssl"
+sitedir="/var/www/html/adfs"
+sslfile="server"
+apachefile="adfs"
+sudo mkdir -p $ssldir
+sudo openssl genpkey -algorithm RSA -out $ssldir/$sslfile.key -pkeyopt rsa_keygen_bits:2048
+sudo openssl req -new -key $ssldir/$sslfile.key -out $ssldir/$sslfile.csr
+sudo openssl x509 -req -days 365 -in $ssldir/$sslfile.csr -signkey $ssldir/$sslfile.key -out $ssldir/$sslfile.crt
+cat >> /etc/apache2/sites-available/$apachefile.conf << EOL
 <VirtualHost *:80>
-    ServerName lamp.migrate.local
-    Redirect permanent / https://lamp.migrate.local/
+    ServerName $domain
+    Redirect permanent / https://$domain/
 </VirtualHost>
 
 <VirtualHost *:443>
-    ServerName lamp.migrate.local
+    ServerName $domain
 
-    DocumentRoot /var/www/html/adfs
+    DocumentRoot $sitedir
     ErrorLog ${APACHE_LOG_DIR}/error.log
     CustomLog ${APACHE_LOG_DIR}/access.log combined
 
     SSLEngine on
-    SSLCertificateFile /etc/apache2/ssl/server.crt
-    SSLCertificateKeyFile /etc/apache2/ssl/server.key
+    SSLCertificateFile $ssldir/$crtname.crt
+    SSLCertificateKeyFile $ssldir/$crtname.key
 
-    <Directory /var/www/html/adfs>
+    <Directory $sitedir>
         Options Indexes FollowSymLinks
         AllowOverride All
         Require all granted
     </Directory>
 </VirtualHost>
-
+EOL
 sudo a2enmod ssl
-sudo a2ensite adfs.conf
+sudo a2ensite $apachefile.conf
 sudo systemctl restart apache2
