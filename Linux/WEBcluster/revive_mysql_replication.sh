a88810d567581e26fcb5cb73e5e5cb9e554d9608
Vitalii <vitalii.stepchuk@sidmar.ch>
2024-07-11 18:47:17 +0300

    Update revive_mysql_replication.sh

--- Linux/WEBcluster/revive_mysql_replication.sh
+++ Linux/WEBcluster/revive_mysql_replication.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# crontab -l | { cat; echo "*/1 * * * * /etc/keepalived/revive_mysql_replication.sh"; } | crontab
+# GRANT ALL PRIVILEGES ON *.* TO 'root'@'<ip_server2>' IDENTIFIED BY 'root';
+#FLUSH PRIVILEGES;
+# GRANT ALL PRIVILEGES ON *.* TO 'root'@'<ip_server1>' IDENTIFIED BY 'root';
+# FLUSH PRIVILEGES;
 LOCAL_FILE=`mysql --user=root --password=root -e "show slave status \G" | grep "Master_Log_File" | awk ' {print $2}'`
 REMOTE_FILE=`mysql --host=<IP адрес встречного сервера> --user=root --password=root -e "show master status \G" | grep "File" | awk '{print $2}'`
  
