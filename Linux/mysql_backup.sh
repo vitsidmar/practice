6b480f7adce547ac8b32e67b08275d45e8602d79
Vitalii <vitalii.stepchuk@sidmar.ch>
2024-07-11 17:36:40 +0300

    Update mysql_backup.sh

--- Linux/mysql_backup.sh
+++ Linux/mysql_backup.sh
@@ -1,26 +1,53 @@
 #!/bin/bash
-
-# Database credentials
-DB_USER="your_username"
-DB_PASSWORD="your_password"
-DB_NAME="your_database_name"
-
-# Backup directory
-BACKUP_DIR="/path/to/backup/directory"
-
-# Timestamp (to create unique backup filenames)
-TIMESTAMP=$(date +"%Y%m%d%H%M%S")
-
-# Create backup directory if it doesn't exist
+# Datenbank Parameter
+DB_HOST="localhost"
+DB_USER="sqluser"
+DB_PASSWORD="pass"
+DB_NAME="komments"
+# Samba Server Parameter
+SAMBA_SERVER="//172.16.240.29/daten/sql/"
+MOUNT_POINT="/mnt/sharedfolder"
+SAMBA_USERNAME="vital"
+SAMBA_PASSWORD="Password1"
+# Local Parameter
+BACKUP_DIR="/root/sql_backup"
+CURRENT_DATE=$(date +"%Y-%m-%d_%H:%M:%S")
+HOSTNAME=$(hostname -s)
+BACKUP_FILE="$BACKUP_DIR/${CURRENT_DATE}_${DB_NAME}_${HOSTNAME}.sql"
 mkdir -p $BACKUP_DIR
 
-# Backup the MySQL database
-mysqldump -u$DB_USER -p$DB_PASSWORD $DB_NAME > $BACKUP_DIR/$DB_NAME-$TIMESTAMP.sql
+mysqldump -h $DB_HOST -u $DB_USER -p$DB_PASSWORD $DB_NAME > $BACKUP_FILE
 
-# Compress the backup
-gzip $BACKUP_DIR/$DB_NAME-$TIMESTAMP.sql
+if [ $? -eq 0 ]; then
+    echo "Datenbank-Backup $DB_NAME erfolgreich erstellt: $BACKUP_FILE"
+    gzip $BACKUP_FILE
+    # Überprüfen, ob die Samba-Verbindung aktiv ist
+    if mountpoint -q $MOUNT_POINT; then
+        echo "Samba-Server bereits verbunden."
+    else
+        # Verbindung zum Samba-Server herstellen
+        mkdir -p $MOUNT_POINT
+        sudo mount -t cifs $SAMBA_SERVER $MOUNT_POINT -o username=$SAMBA_USERNAME,password=$SAMBA_PASSWORD
+        if [ $? -eq 0 ]; then
+            echo "Verbindung zum Samba-Server erfolgreich hergestellt."
+        else
+            echo "Fehler bei der Verbindung zum Samba-Server."
+            exit 1
+        fi
+    fi
 
-# Optionally, you can remove backups older than a certain period
+    # Kopieren des Backups auf den Samba-Server
+    cp $BACKUP_FILE.gz $MOUNT_POINT
+    if [ $? -eq 0 ]; then
+        echo "Backup erfolgreich auf den Samba-Server kopiert: $BACKUP_FILE"
+    else
+        echo "Fehler beim Kopieren des Backups auf den Samba-Server."
+    fi
+else
+    echo "Fehler beim Erstellen des Datenbank-Backups $DB_NAME"
+fi
+
+# Löschen von .gz-Dateien, die älter als 7 Tage sind
 # find $BACKUP_DIR -type f -name "*.gz" -mtime +7 -exec rm {} \;
 
-echo "Backup completed: $BACKUP_DIR/$DB_NAME-$TIMESTAMP.sql.gz"
+echo "Backup completed: $BACKUP_FILE"
